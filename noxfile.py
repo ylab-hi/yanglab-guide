3c3c6f16181ac8260ccac7d90094e04ff995f0c4
yangli <li002252@umn.edu>
2022-05-01 11:14:02 -0500

    add github actions

---
+++ noxfile.py
@@ -0,0 +1,89 @@
+"""Nox sessions."""
+import shutil
+from pathlib import Path
+
+import nox
+from nox.sessions import Session
+
+nox.options.sessions = ["docs"]
+owner, repository = "ylab-hi", "yanglab-guide"
+labels = "labguide", "documentation"
+bump_paths = (
+    "README.md",
+    "source/index.md",
+    "source/developguide.md",
+    "source/library.md",
+)
+
+
+@nox.session(name="prepare-release")
+def prepare_release(session: Session) -> None:
+    """Prepare a GitHub release."""
+    args = [
+        f"--owner={owner}",
+        f"--repository={repository}",
+        *[f"--bump={path}" for path in bump_paths],
+        *[f"--label={label}" for label in labels],
+        *session.posargs,
+    ]
+    session.install("click", "github3.py")
+    session.run("python", "tools/prepare-github-release.py", *args, external=True)
+
+
+@nox.session(name="publish-release")
+def publish_release(session: Session) -> None:
+    """Publish a GitHub release."""
+    args = [f"--owner={owner}", f"--repository={repository}", *session.posargs]
+    session.install("click", "github3.py")
+    session.run("python", "tools/publish-github-release.py", *args, external=True)
+
+
+nox.options.sessions = ["linkcheck"]
+
+
+@nox.session
+def docs(session: Session) -> None:
+    """Build the documentation."""
+    args = session.posargs or ["-W", "-n", "source", "source/_build"]
+
+    if session.interactive and not session.posargs:
+        args = ["-a", "--watch=source/_static", "--open-browser", *args]
+
+    builddir = Path("source", "_build")
+    if builddir.exists():
+        shutil.rmtree(builddir)
+
+    session.install("-r", "source/requirements.txt")
+
+    if session.interactive:
+        session.run("sphinx-autobuild", *args)
+    else:
+        session.run("sphinx-build", *args)
+
+
+@nox.session
+def linkcheck(session: Session) -> None:
+    """Build the documentation."""
+    args = session.posargs or [
+        "-b",
+        "linkcheck",
+        "-W",
+        "--keep-going",
+        "source",
+        "source/_build",
+    ]
+
+    builddir = Path("docs", "_build")
+    if builddir.exists():
+        shutil.rmtree(builddir)
+
+    session.install("-r", "source/requirements.txt")
+
+    session.run("sphinx-build", *args)
+
+
+@nox.session(name="dependencies-table")
+def dependencies_table(session: Session) -> None:
+    """Print the dependencies table."""
+    session.install("tomli")
+    session.run("python", "tools/dependencies-table.py", external=True)
