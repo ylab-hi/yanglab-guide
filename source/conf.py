ab69d31d2a254cbe2550ca98d75a2c13a3722582
yangli <li002252@umn.edu>
2022-05-01 12:02:53 -0500

    add links

--- source/conf.py
+++ source/conf.py
@@ -31,8 +31,24 @@ release = "1.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser", "sphinx.ext.intersphinx"]
-intersphinx_mapping = {"mypy": ("https://mypy.readthedocs.io/en/stable/", None)}
+extensions = [
+    "myst_parser",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinx_tabs.tabs",
+    "sphinx_thebe",
+    "sphinx_togglebutton",
+    "sphinxcontrib.bibtex",
+    "sphinxcontrib.youtube",
+    "sphinxext.opengraph",
+]
+intersphinx_mapping = {
+    "mypy": ("https://mypy.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3.8", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master", None),
+}
 source_suffix = [".rst", ".md"]
 language = "en"
 linkcheck_ignore = [
@@ -54,6 +70,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinx_book_theme"
+html_title = "Lab Guide For Yang Lab"
+html_copy_source = True
 html_theme_options = {
     "repository_url": "https://github.com/ylab-hi/yanglab-guide",
     "use_repository_button": True,
@@ -64,3 +82,35 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_logo = "_static/logo.png"
+
+
+html_theme_options = {
+    "path_to_docs": "source",
+    "repository_url": "https://github.com/ylab-hi/yanglab-guide",
+    # "repository_branch": "gh-pages",  # For testing
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+        "colab_url": "https://colab.research.google.com/",
+        "deepnote_url": "https://deepnote.com/",
+        "notebook_interface": "jupyterlab",
+        "thebe": True,
+        # "jupyterhub_url": "https://datahub.berkeley.edu",  # For testing
+    },
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    "use_repository_button": True,
+    "use_download_button": True,
+    "logo_only": True,
+    "show_toc_level": 2,
+    "announcement": (
+        "⚠️The latest release refactored our HTML, "
+        "so double-check your custom CSS rules!⚠️"
+    ),
+    # For testing
+    # "use_fullscreen_button": False,
+    # "home_page_in_toc": True,
+    # "single_page": True,
+    # "extra_footer": "<a href='https://google.com'>Test</a>",  # DEPRECATED KEY
+    # "extra_navbar": "<a href='https://google.com'>Test</a>",
+    # "show_navbar_depth": 2,
+}
