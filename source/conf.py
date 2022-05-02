0f0f622c93baeecc668e9541561db91c66545ca5
yangli <li002252@umn.edu>
2022-05-01 22:22:04 -0500

    add grid layout

--- source/conf.py
+++ source/conf.py
@@ -17,6 +17,17 @@
 
 # -- Project information -----------------------------------------------------
 from datetime import datetime
+from pathlib import Path
+import random
+from textwrap import dedent
+from urllib.parse import urlparse
+
+import yaml
+
+from sphinx.application import Sphinx
+from sphinx.util import logging
+
+LOGGER = logging.getLogger("conf")
 
 project = "yanglab-guide"
 author = "Yangang Li"
@@ -117,3 +128,76 @@ html_theme_options = {
 
 
 bibtex_bibfiles = ["references.bib"]
+
+
+def build_gallery(app: Sphinx):
+    # Build the gallery file
+    LOGGER.info("building gallery...")
+    grid_items = []
+    projects = yaml.safe_load((Path(app.srcdir) / "gallery.yml").read_text())
+    random.shuffle(projects)
+    for item in projects:
+        if not item.get("image"):
+            item["image"] = "https://jupyterbook.org/_images/logo-square.svg"
+
+        repo_text = ""
+        star_text = ""
+
+        if item["repository"]:
+            repo_text = f'{{bdg-link-secondary}}`repo <{item["repository"]}>`'
+
+            try:
+                url = urlparse(item["repository"])
+                if url.netloc == "github.com":
+                    _, org, repo = url.path.rstrip("/").split("/")
+                    star_text = f"[![GitHub Repo stars](https://img.shields.io/github/stars/{org}/{repo}?style=social)]({item['repository']})"
+            except Exception as error:
+                LOGGER.warning(f"failed to parse repository url: {error}")
+                pass
+
+        grid_items.append(
+            f"""\
+        `````{{grid-item-card}} {" ".join(item["name"].split())}
+        :text-align: center
+        <img src="{item["image"]}" alt="logo" loading="lazy" style="max-width: 100%; max-height: 200px; margin-top: 1rem;" />
+        +++
+        ````{{grid}} 2 2 2 2
+        :margin: 0 0 0 0
+        :padding: 0 0 0 0
+        :gutter: 1
+        ```{{grid-item}}
+        :child-direction: row
+        :child-align: start
+        :class: sd-fs-5
+        {{bdg-link-secondary}}`website <{item["website"]}>`
+        {repo_text}
+        ```
+        ```{{grid-item}}
+        :child-direction: row
+        :child-align: end
+        {star_text}
+        ```
+        ````
+        `````
+        """
+        )
+    grid_items = "\n".join(grid_items)
+
+    # :column: text-center col-6 col-lg-4
+    # :card: +my-2
+    # :img-top-cls: w-75 m-auto p-2
+    # :body: d-none
+
+    panels = f"""
+``````{{grid}} 1 2 3 3
+:gutter: 1 1 2 2
+:class-container: full-width
+{dedent(grid_items)}
+``````
+    """
+    (Path(app.srcdir) / "gallery.txt").write_text(panels)
+
+
+def setup(app: Sphinx):
+    app.add_css_file("custom.css")
+    app.connect("builder-inited", build_gallery)
--- source/library.md
+++ source/library.md
@@ -1,3 +1,8 @@
-# Library
+# Lab Library
 
-This include all books our lab have
+This is a gallery of Jupyter Books built from across the community.
+If you'd like to add your book to this list, simply [add an entry to this `gallery.yml` file](https://github.com/executablebooks/meta/edit/master/docs/gallery.yml) and open a Pull Request to add it.
+
+```{include} gallery.txt
+
+```
